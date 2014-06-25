6d0da5647c8031712d88c641fb8b31f70e3577be
faouellet <faouellet@gmail.com>
2014-06-25 19:55:56 -0400

    Adding a working printer

--- CPP/templatesort.cpp
+++ CPP/templatesort.cpp
@@ -2,49 +2,28 @@
 #include <tuple>
 #include <type_traits>
 
-/* template<class Op>
-struct filter
+template<int... Tail> struct StaticIntList { }; 
+
+template<class T> struct Printer;
+
+template<int Head, int... Tail>
+struct Printer<StaticIntList<Head, Tail...>>
 {
-	using type = std::tuple<>;
+    static void print()
+    {
+        std::cout << Head << std::endl;
+        Printer<StaticIntList<Tail...>>::print();
+    }
 };
 
-template<class Op, int Head, int... Tail>
-struct filter
+template<>
+struct Printer<StaticIntList<>>
 {
-	using type = std::conditional<Op<Head>::value, 
-				 merge<Head, typename filter<Tail...>::type>,
-				 typename filter<Tail...>::type
-				 >::type;
+    static void print() { }
 };
-
-template<typename... Args>
-struct merge
-{
-	using type = std::tuple<Args...>;
-}; */
-
-template<int I>
-struct IsNotNegative
-{
-	using value = I > 0;
-};
-
-template<class Tuple, size_t Pos>
-std::enable_if<IsNotNegative<Pos>::value, std::ostream&>
-Print(std::ostream& os, const Tuple& t)
-{
-
-}
-
-template<class... Args>
-std::ostream& operator<<(ostream& os, const std::tuple<Args...>& tuple)
-{
-	Print<decltype<tuple>, std::tuple_size<decltype<tuple>>>(os, tuple);
-}
 
 int main()
 {
-	std::tuple<1,2,3> Tuple;
-	
-	std::cout << Tuple << std::endl;
+    using SIL = StaticIntList<4,3,2,1>;
+    Printer<SIL>::print();
 }
