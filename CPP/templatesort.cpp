cda38273436009c242f6338f6b13e39ee63f4070
faouellet <faouellet@gmail.com>
2014-06-25 20:48:20 -0400

    Adding concatenation

--- CPP/templatesort.cpp
+++ CPP/templatesort.cpp
@@ -1,10 +1,17 @@
+/*
+ * Implementation of a quicksort for integers using template metaprogramming
+ * */
+
 #include <iostream>
 #include <tuple>
 #include <type_traits>
 
-template<int... Tail> struct StaticIntList { }; 
+// Compile-time representation of a list of integers
+template<int... Vals> struct StaticIntList { };
 
-template<class T> struct Printer;
+////////////////////////////////////////////////////////////////////////////////
+// Printing
+template<class> struct Printer;
 
 template<int Head, int... Tail>
 struct Printer<StaticIntList<Head, Tail...>>
@@ -21,9 +28,41 @@ struct Printer<StaticIntList<>>
 {
     static void print() { }
 };
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Prepend
+template<int, class> struct Prepend;
+
+template<int Val, int... Vals>
+struct Prepend<Val, StaticIntList<Vals...>>
+{
+    using type = StaticIntList<Val, Vals...>;
+};
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Concatenation
+template<class, class> struct Concat;
+
+template<int Head, int... Tail1, int... Tail2>
+struct Concat<StaticIntList<Head, Tail1...>, StaticIntList<Tail2...>>
+{
+    using type = typename Prepend<Head, 
+        typename Concat<StaticIntList<Tail1...>, StaticIntList<Tail2...>>::type
+        >::type; 
+};
+
+template<int... Tail>
+struct Concat<StaticIntList<>, StaticIntList<Tail...>>
+{
+    using type = StaticIntList<Tail...>; 
+};
+////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    using SIL = StaticIntList<4,3,2,1>;
-    Printer<SIL>::print();
+    using SIL1 = StaticIntList<4,3,2,1>;
+    using SIL2 = StaticIntList<4,3,2,1>;
+    Printer<typename Concat<SIL1, SIL2>::type>::print();
 }
