78784377d3b49de66d484d6fb3849ed8695ecbba
faouellet <faouellet@gmail.com>
2018-02-22 19:49:36 -0500

    Completed benchmarking script

--- Interpreter/benchmark.py
+++ Interpreter/benchmark.py
@@ -4,28 +4,32 @@ from subprocess import Popen, PIPE
 from matplotlib.ticker import FuncFormatter
 import matplotlib.pyplot as plt
 
+import pdb
+
 result_re = re.compile("Mean elapsed time: (?P<time>[0-9]+)")
 
 cpp_files = [f for f in os.listdir(os.getcwd()) if f.endswith(".cpp")]
+programs = [os.path.splitext(p)[0] for p in cpp_files]
 
-for f in cpp_files:
-    compile_proc = Popen(["g++", "-O2", "-o", os.path.splitext(f)[0], f])
+for i in range(len(cpp_files)):
+    print "Compiling {0}".format(cpp_files[i])
+    compile_proc = Popen(["g++", "-O2", "-o", programs[i], cpp_files[i]])
     assert(compile_proc.wait() == 0)
 
-#programs = ["is", "ds", "loop", "it", "dt"]
-#results = { "is":10, "loop":9, "ds":6, "it":2, "dt":0.5 }
+results = {}
 
-#for p in programs:
-#    process = Popen([p], stdout=PIPE)
-#    (output, err) = process.communicate()
-#    exit_code = process.wait()
-#
-#    match = result_re.match(output)
-#    results[p] = match["time"]
+for p in programs:
+    print "Running {0}".format(p)
+    process = Popen(["./{0}".format(p)], stdout=PIPE)
+    (output, err) = process.communicate()
+    assert(process.wait() == 0)
 
-#x = np.arange(len(results))
-#
-#fig, ax = plt.subplots()
-#plt.bar(x, sorted(results.values(), reverse=True), align='center')
-#plt.xticks(x, sorted(results, reverse=True, key=results.get))
-#plt.show()
+    match = result_re.match(output)
+    results[p] = match.group("time")
+
+results_range = range(len(results))
+
+fig, ax = plt.subplots()
+plt.bar(results_range, sorted(results.values(), reverse=True), align='center')
+plt.xticks(results_range, sorted(results, reverse=True, key=results.get))
+plt.show()
