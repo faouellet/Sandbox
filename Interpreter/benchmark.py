756ed24960287b8864f347744b1ace0ee1d9c651
faouellet <faouellet@gmail.com>
2018-02-22 19:17:13 -0500

    Interpreter benchmark script now compiles the programs to run

--- Interpreter/benchmark.py
+++ Interpreter/benchmark.py
@@ -1,13 +1,19 @@
+import os
 import re
 from subprocess import Popen, PIPE
 from matplotlib.ticker import FuncFormatter
 import matplotlib.pyplot as plt
-import numpy as np
 
 result_re = re.compile("Mean elapsed time: (?P<time>[0-9]+)")
 
-programs = ["is", "ds", "loop", "it", "dt"]
-results = { "is":10, "loop":9, "ds":6, "it":2, "dt":0.5 }
+cpp_files = [f for f in os.listdir(os.getcwd()) if f.endswith(".cpp")]
+
+for f in cpp_files:
+    compile_proc = Popen(["g++", "-O2", "-o", os.path.splitext(f)[0], f])
+    assert(compile_proc.wait() == 0)
+
+#programs = ["is", "ds", "loop", "it", "dt"]
+#results = { "is":10, "loop":9, "ds":6, "it":2, "dt":0.5 }
 
 #for p in programs:
 #    process = Popen([p], stdout=PIPE)
@@ -17,9 +23,9 @@ results = { "is":10, "loop":9, "ds":6, "it":2, "dt":0.5 }
 #    match = result_re.match(output)
 #    results[p] = match["time"]
 
-x = np.arange(len(results))
-
-fig, ax = plt.subplots()
-plt.bar(x, sorted(results.values(), reverse=True), align='center')
-plt.xticks(x, sorted(results, reverse=True, key=results.get))
-plt.show()
+#x = np.arange(len(results))
+#
+#fig, ax = plt.subplots()
+#plt.bar(x, sorted(results.values(), reverse=True), align='center')
+#plt.xticks(x, sorted(results, reverse=True, key=results.get))
+#plt.show()
